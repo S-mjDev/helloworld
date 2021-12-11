ec34a2232499ac87c0400acb8a99ee4ff5724960
MarkJude <markjudesurio@gmail.com>
2021-12-12 06:13:51 +0800

    My fisrt server

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const { listenerCount } = require("process");
+
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My fisrt server!")
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () =>{
+    console.log(`Server is running on http://${host}:${port}`);
+});
